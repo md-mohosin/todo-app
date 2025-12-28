691fea6538ae97802d61a89fc374123566264648
md mohosin <mdmohosin.web@gmail.com>
2025-12-28 16:51:26 +0600

    add new task

--- src/todo.js
+++ src/todo.js
@@ -11,18 +11,67 @@ let todos = [
 
 
 const todosContainer = document.getElementById('todos')
+const input = document.getElementById("new-todo");
 
-const singletodo = todos.map(todo => {
+
+function renderTodos() {
+  todosContainer.innerHTML = ''
+
+  todos.forEach((todo, index) => {
+
     const div = document.createElement("div")
+    div.className = 'singleTodo'
 
-    div.innerHTML=`
-    <div class="singleTodo">
-        <div class="toto-title">
-          <input type="checkbox" name="" id="">
-          <p>${todo.name}</p>
-        </div>
-        <img src="./src/images/trash.png" alt="">
-      </div>
-    `
+    const todoTitle = document.createElement("div");
+    todoTitle.className = "toto-title"
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+
+    const text = document.createElement("p");
+    text.textContent = todo.name;
+
+    todoTitle.appendChild(checkbox)
+    todoTitle.appendChild(text)
+
+    div.appendChild(todoTitle)
+
     todosContainer.appendChild(div)
+  })
+}
+
+
+
+
+function hide(id) {
+  document.getElementById(id).style.display = 'none'
+}
+function show(id) {
+  document.getElementById(id).style.display = 'block'
+}
+
+
+document.getElementById("add").addEventListener("click", () => {
+  hide('todos')
 })
+
+
+
+document.getElementById("add-btn").addEventListener("click", (e) => {
+  e.preventDefault()
+  const inputValue = input.value;
+  if (!inputValue.trim()) {
+    alert("Please valid input value")
+    return
+  }
+  const newTodo = {
+    name: inputValue,
+    completed: false
+  }
+  todos.push(newTodo)
+  input.value = ''
+  renderTodos()
+})
+
+renderTodos()
